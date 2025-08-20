575ac1a6bb6c62c767eeed8973f7ce0ad40412c2
gholiveira29 <g.h.p.o3615@gmail.com>
2025-08-20 18:08:27 -0300

    thead com java puro

--- src/br/com/alura/AppBanco.java
+++ src/br/com/alura/AppBanco.java
@@ -7,11 +7,22 @@ public class AppBanco {
 
         var cliente = new Cliente("Talita");
         var conta = new Conta(cliente, new BigDecimal(2000));
-
-
         var operacao = new OperacaoSaque(conta,new BigDecimal(2000));
 
-        operacao.executa();
-        operacao.executa();
+        Thread thread1 = new Thread(operacao);
+        Thread thread2 = new Thread(operacao);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        System.out.println("Saldo final: "+ conta.getSaldo());
     }
 }
